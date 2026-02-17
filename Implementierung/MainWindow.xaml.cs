52a7891f931295101635dc2015669b9a7ef7332f
schneiderleon <150476198+schneiderleon@users.noreply.github.com>
2026-02-17 14:27:56 +0100

    GUI für Paketverfolgung
    Hier wird alleine nur die GUI hochgeladen, nicht der Code behind

---
+++ Implementierung/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PaketverfolgungLS
+{
+    
+    public partial class MainWindow : Window
+    {
+
+        private void BtnSearch_Click(object sender, RoutedEventArgs e)
+        {
+            return;
+        }
+
+        private void BtnReset_Click(object sender, RoutedEventArgs e)
+        {
+            return;
+        }
+
+        private void BtnNewOrder_Click(object sender, RoutedEventArgs e)
+        {
+            return;
+        }
+
+        private void BtnNewCustomer_Click(object sender, RoutedEventArgs e)
+        {
+            return;
+        }
+
+        private void BtnEditOrder_Click(object sender, RoutedEventArgs e)
+        {
+           return;
+        }
+
+        private void BtnSaveOrder_Click(object sender, RoutedEventArgs e)
+        {
+            return;
+        }
+
+        private void BtnCancelOrder_Click(object sender, RoutedEventArgs e)
+        {
+            return;
+        }
+
+        private void BtnEditCustomer_Click(object sender, RoutedEventArgs e)
+        {
+            return;
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
